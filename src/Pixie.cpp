a996cf979e55731689f361f34373abec1a7a3b98
Lixie Labs <connornishijima@gmail.com>
2020-06-13 12:55:53 -0600

    Icons update

--- src/Pixie.cpp
+++ src/Pixie.cpp
@@ -77,7 +77,35 @@ void Pixie::write(uint32_t input, uint8_t pos){
 }
 
 void Pixie::write(char input, uint8_t pos){
-	set_char(input, pos);
+	write_char(input, pos);
+}
+
+void Pixie::write(uint8_t* icon, uint8_t pos) {
+	if(pos < disp_count){		
+		display_buffer[8*pos+0] = 0;
+		display_buffer[8*pos+1] = bright;
+		display_buffer[8*pos+2] = 0;
+		
+		display_buffer[8*pos+3] = icon[0];
+		display_buffer[8*pos+4] = icon[1];
+		display_buffer[8*pos+5] = icon[2];
+		display_buffer[8*pos+6] = icon[3];
+		display_buffer[8*pos+7] = icon[4];
+	}
+}
+
+void Pixie::write(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5, uint8_t pos){
+	if(pos < disp_count){		
+		display_buffer[8*pos+0] = 0;
+		display_buffer[8*pos+1] = bright;
+		display_buffer[8*pos+2] = 0;
+		
+		display_buffer[8*pos+3] = byte1;
+		display_buffer[8*pos+4] = byte2;
+		display_buffer[8*pos+5] = byte3;
+		display_buffer[8*pos+6] = byte4;
+		display_buffer[8*pos+7] = byte5;
+	}
 }
 
 void Pixie::write(char* input, uint8_t pos){
@@ -86,7 +114,25 @@ void Pixie::write(char* input, uint8_t pos){
 		len = disp_count-pos;
 	}
 	for(uint8_t i = 0; i < len; i++){
-		set_char(input[i], i+pos);
+		write_char(input[i], i+pos);
+	}
+}
+
+void Pixie::write_char(char chr, uint8_t pos) {
+	if(pos < disp_count){
+		if (chr >= 32) {
+			chr -= 32;
+		}
+		
+		display_buffer[8*pos+0] = 0;
+		display_buffer[8*pos+1] = bright;
+		display_buffer[8*pos+2] = 0;
+		
+		display_buffer[8*pos+3] = pgm_read_byte_far(col+(chr * 5 + 0));
+		display_buffer[8*pos+4] = pgm_read_byte_far(col+(chr * 5 + 1));
+		display_buffer[8*pos+5] = pgm_read_byte_far(col+(chr * 5 + 2));
+		display_buffer[8*pos+6] = pgm_read_byte_far(col+(chr * 5 + 3));
+		display_buffer[8*pos+7] = pgm_read_byte_far(col+(chr * 5 + 4));
 	}
 }
 
@@ -127,11 +173,180 @@ void Pixie::push(char input){
 	push_char(input);
 }
 
+void Pixie::push(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5){
+	push_byte(0);
+	push_byte(bright);
+	push_byte(0);
+	push_byte(byte1);
+	push_byte(byte2);
+	push_byte(byte3);
+	push_byte(byte4);
+	push_byte(byte5);
+}
+
+void Pixie::push(uint8_t* icon){
+	push_byte(0);
+	push_byte(bright);
+	push_byte(0);
+	push_byte(icon[0]);
+	push_byte(icon[1]);
+	push_byte(icon[2]);
+	push_byte(icon[3]);
+	push_byte(icon[4]);
+}
+
 void Pixie::push(char* input){
 	uint8_t len = strlen(input);
 	for(uint8_t i = 0; i < len; i++){
 		push_char(input[i]);
 	}
+}
+
+void Pixie::push_byte(uint8_t col) {
+	for (uint16_t i = 0; i < disp_count * 8 - 1; i++) {
+		display_buffer[i] = display_buffer[i + 1];
+	}
+	display_buffer[disp_count * 8 - 1] = col;
+}
+
+void Pixie::push_char(char chr) {
+	if (chr >= 32) {
+		chr -= 32;
+	}
+
+	push_byte(0);
+	push_byte(bright);
+	push_byte(0);
+	push_byte(pgm_read_byte_far(col+(chr * 5 + 0)));
+	push_byte(pgm_read_byte_far(col+(chr * 5 + 1)));
+	push_byte(pgm_read_byte_far(col+(chr * 5 + 2)));
+	push_byte(pgm_read_byte_far(col+(chr * 5 + 3)));
+	push_byte(pgm_read_byte_far(col+(chr * 5 + 4)));
+}
+
+void Pixie::shift(float input, uint8_t places){
+	const uint8_t len = get_length((float)input, places);
+	char char_buf[len];
+	sprintf(char_buf, "%.*f", places, input);
+	for(uint8_t i = 0; i < len; i++){
+		shift_char(char_buf[len-1-i]);
+	}
+}
+
+void Pixie::shift(int32_t input){
+	const uint8_t len = get_length((int32_t)input);
+	char char_buf[len];
+	ltoa(input,char_buf,10);
+	for(uint8_t i = 0; i < len; i++){
+		shift_char(char_buf[len-1-i]);
+	}
+}
+
+void Pixie::shift(uint32_t input){
+	const uint8_t len = get_length(input);
+	char char_buf[len];
+	ultoa(input,char_buf,10);
+	for(uint8_t i = 0; i < len; i++){
+		shift_char(char_buf[len-1-i]);
+	}
+}
+
+void Pixie::shift(char input){
+	shift_char(input);
+}
+
+void Pixie::shift(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5){
+	shift_byte(byte5);
+	shift_byte(byte4);
+	shift_byte(byte3);
+	shift_byte(byte2);
+	shift_byte(byte1);
+	shift_byte(0);
+	shift_byte(bright);
+	shift_byte(0);
+}
+
+void Pixie::shift(uint8_t* icon){
+	shift_byte(icon[4]);
+	shift_byte(icon[3]);
+	shift_byte(icon[2]);
+	shift_byte(icon[1]);
+	shift_byte(icon[0]);
+	shift_byte(0);
+	shift_byte(bright);
+	shift_byte(0);
+}
+
+void Pixie::shift(char* input){
+	uint8_t len = strlen(input);
+	for(int16_t i = len-1; i >= 0; i--){
+		shift_char(input[i]);
+	}
+}
+
+void Pixie::shift_byte(uint8_t col) {
+	for (uint16_t i = disp_count * 8-1; i > 0; i--) {
+		display_buffer[i] = display_buffer[i - 1];
+	}
+	display_buffer[0] = col;
+}
+
+void Pixie::shift_char(char chr) {
+	if (chr >= 32) {
+		chr -= 32;
+	}
+
+	shift_byte(pgm_read_byte_far(col+(chr * 5 + 4)));
+	shift_byte(pgm_read_byte_far(col+(chr * 5 + 3)));
+	shift_byte(pgm_read_byte_far(col+(chr * 5 + 2)));
+	shift_byte(pgm_read_byte_far(col+(chr * 5 + 1)));
+	shift_byte(pgm_read_byte_far(col+(chr * 5 + 0)));
+	shift_byte(0);
+	shift_byte(bright);
+	shift_byte(0);
+}
+
+uint8_t Pixie::get_length(float input, uint8_t dec){
+	if(input == 0){
+		return 2+dec;
+	}
+	uint8_t places = 1 + dec;
+	if(input < 0){
+		places++; // signed
+	}
+	int32_t input_whole = input;
+	while(abs(input) >= 1){
+		input /= 10;
+		places++;
+	}
+	return places;
+}
+
+uint8_t Pixie::get_length(int32_t input){
+	if(input == 0){
+		return 1;
+	}
+	uint8_t places = 0;
+	if(input < 0){
+		places++; // signed
+	}
+	while(abs(input) >= 1){
+		input /= 10;
+		places++;
+	}
+	return places;
+}
+
+uint8_t Pixie::get_length(uint32_t input){
+	if(input == 0){
+		return 1;
+	}
+	uint8_t places = 0;
+	while(abs(input) >= 1){
+		input /= 10;
+		places++;
+	}
+	return places;
 }
 
 void Pixie::scroll_message(char* input, uint16_t wait_ms, bool instant){
@@ -170,96 +385,6 @@ void Pixie::scroll_message(char* input, uint16_t wait_ms, bool instant){
 			}
 			delay(150);
 		}
-	}
-}
-
-void Pixie::push_byte(uint8_t col) {
-	for (uint16_t i = 0; i < disp_count * 8 - 1; i++) {
-		display_buffer[i] = display_buffer[i + 1];
-	}
-	display_buffer[disp_count * 8 - 1] = col;
-}
-
-void Pixie::push_char(char chr) {
-	if (chr >= 32) {
-		chr -= 32;
-	}
-
-	push_byte(0);
-	push_byte(bright);
-	push_byte(0);
-	push_byte(pgm_read_byte_far(col+(chr * 5 + 0)));
-	push_byte(pgm_read_byte_far(col+(chr * 5 + 1)));
-	push_byte(pgm_read_byte_far(col+(chr * 5 + 2)));
-	push_byte(pgm_read_byte_far(col+(chr * 5 + 3)));
-	push_byte(pgm_read_byte_far(col+(chr * 5 + 4)));
-}
-
-void Pixie::push_icon(uint8_t* icon){
-	push_byte(0);
-	push_byte(bright);
-	push_byte(0);
-	push_byte(icon[0]);
-	push_byte(icon[1]);
-	push_byte(icon[2]);
-	push_byte(icon[3]);
-	push_byte(icon[4]);
-}
-
-void Pixie::push_icon(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5){
-	push_byte(0);
-	push_byte(bright);
-	push_byte(0);
-	push_byte(byte1);
-	push_byte(byte2);
-	push_byte(byte3);
-	push_byte(byte4);
-	push_byte(byte5);
-}
-
-void Pixie::set_char(char chr, uint8_t pos) {
-	if(pos < disp_count){
-		if (chr >= 32) {
-			chr -= 32;
-		}
-		
-		display_buffer[8*pos+0] = 0;
-		display_buffer[8*pos+1] = bright;
-		display_buffer[8*pos+2] = 0;
-		
-		display_buffer[8*pos+3] = pgm_read_byte_far(col+(chr * 5 + 0));
-		display_buffer[8*pos+4] = pgm_read_byte_far(col+(chr * 5 + 1));
-		display_buffer[8*pos+5] = pgm_read_byte_far(col+(chr * 5 + 2));
-		display_buffer[8*pos+6] = pgm_read_byte_far(col+(chr * 5 + 3));
-		display_buffer[8*pos+7] = pgm_read_byte_far(col+(chr * 5 + 4));
-	}
-}
-
-void Pixie::set_icon(uint8_t* icon, uint8_t pos) {
-	if(pos < disp_count){		
-		display_buffer[8*pos+0] = 0;
-		display_buffer[8*pos+1] = bright;
-		display_buffer[8*pos+2] = 0;
-		
-		display_buffer[8*pos+3] = icon[0];
-		display_buffer[8*pos+4] = icon[1];
-		display_buffer[8*pos+5] = icon[2];
-		display_buffer[8*pos+6] = icon[3];
-		display_buffer[8*pos+7] = icon[4];
-	}
-}
-
-void Pixie::set_icon(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5, uint8_t pos){
-	if(pos < disp_count){		
-		display_buffer[8*pos+0] = 0;
-		display_buffer[8*pos+1] = bright;
-		display_buffer[8*pos+2] = 0;
-		
-		display_buffer[8*pos+3] = byte1;
-		display_buffer[8*pos+4] = byte2;
-		display_buffer[8*pos+5] = byte3;
-		display_buffer[8*pos+6] = byte4;
-		display_buffer[8*pos+7] = byte5;
 	}
 }
 
--- src/Pixie.h
+++ src/Pixie.h
@@ -14,27 +14,41 @@ class Pixie{
     void begin();
 	void show();
 	void brightness(uint8_t b);
-	void clear();
-	void push_byte(uint8_t col);
-	void push_char(char chr);
-	void set_char(char chr, uint8_t pos);
-	void set_icon(uint8_t* icon, uint8_t pos);
-	void set_icon(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5, uint8_t pos);
-	void push_icon(uint8_t* icon);
-	void push_icon(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5);
+	void clear();	
 	
+	void write_char(char input, uint8_t pos = 0);
 	void write(char input,    uint8_t pos = 0);
 	void write(char* input,   uint8_t pos = 0);
 	void write(int32_t input, uint8_t pos = 0);
 	void write(uint32_t input, uint8_t pos = 0);
 	void write(float input, uint8_t places = 2, uint8_t pos = 0);
-	
+	void write(uint8_t* icon, uint8_t pos = 0);
+	void write(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5, uint8_t pos = 0);
+
+	void push_char(char chr);	
+	void push_byte(uint8_t col);
 	void push(char input);
 	void push(char* input);
 	void push(int32_t input);
 	void push(uint32_t input);
 	void push(float input, uint8_t places = 2);
+	void push(uint8_t* icon);
+	void push(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5);
 	
+	void shift_char(char chr);	
+	void shift_byte(uint8_t col);
+	void shift(char input);
+	void shift(char* input);
+	void shift(int32_t input);
+	void shift(uint32_t input);
+	void shift(float input, uint8_t places = 2);
+	void shift(uint8_t* icon);
+	void shift(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5);
+	
+	uint8_t get_length(uint32_t input);
+	uint8_t get_length(int32_t input);
+	uint8_t get_length(float input, uint8_t places);
+		
 	void scroll_message(char* input, uint16_t wait_ms = 150, bool instant = false);
 		
 	void set_pix(uint16_t x, uint16_t y, uint8_t state);
