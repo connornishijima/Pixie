10c1e375847124144326d6e8b76e74fabbfb302d
Lixie Labs <connornishijima@gmail.com>
2020-07-29 18:36:59 -0600

    Converted Raspberry Pi driver to SPI

--- extras/raspberry_pi/example.py
+++ extras/raspberry_pi/example.py
@@ -1,31 +1,25 @@
 #   Pixie for Raspberry Pi Example
 #
-#	UPDATE: This module now has a Python-only mode that avoids C usage.
-#	This mode is slower, but should be more compatible across various platforms
-#	You can experiment with this using the "transfer_mode" variable below.
-#	WIRINGPI FOR PYTHON IS REQUIRED for Python mode, install it using the command:
-#
-#		sudo pip install wiringpi2
-#
 #   Steps to replicate:
 #       1. Add "import pixie_write as pix" to the top of your python code
-#       2. Call pix.write(DAT_pin, CLK_pin, num_pixies, *INPUT*) at any time to write to the displays!
+#       2. Call pix.write(num_pixies, *INPUT*) at any time to write to the displays!
 #
 #   *INPUT* can be any string or number casted to a string, such as str(123).
-#   The two "pixie_write" files MUST be in the same directory as your own code!
-#   
-#   There are three files in this directory:
+#   The "pixie_write.py" file MUST be in the same directory as your own code!
+#
+#   There are two files in this directory:
 #       - "example.py"
 #           This is a stand-in for your own script
 #       - "pixie_write.py"
 #           This script (imported below) converts your input string to a
 #           binary bitstream to be blasted to the Pixies
-#       - "pixie_write" (no file extension)
-#           Compiled machine code that blasts the bitstream to the Pixies
-#   
-#   The DAT_pin and CLK_pin numbers are the WiringPi pinout, NOT the
-#   BCM GPIO pinout, meaning DAT_pin is actually Pin 17, and CLK_pin is 27.
-#   Wiring Pi pinout is here: http://wiringpi.com/pins/
+#
+#   The RasPi Pixie driver uses the SPI pins marked below, and
+#   needs SPI enabled using:
+#
+#     sudo raspi-config
+#
+#   ...and navigating to Interfacing -> SPI to enable it (and reboot)
 #
 #   Hookup Guide:
 #
@@ -37,13 +31,13 @@
 #          X     PIX_GND
 #          X     X   |
 #          X     X   |
+#          X     X   |
+#          X     X   |
+#          X     X   |
+#          X     X   |
 #    PIX_DAT     X   |
+#          X     X   |
 #    PIX_CLK     X   |
-#          X     X   |
-#          X     X   |
-#          X     X   |
-#          X     X   |
-#          X     X   |
 #          X     X   |
 #          X     X   |
 #          X     X   |
@@ -54,12 +48,14 @@
 #          X     X   |
 #                    |
 
+import time
 import pixie_write as pix
-
-DAT_pin = 0
-CLK_pin = 2
 num_pixies = 2
-transfer_mode = "PYTHON" # Can be "C" or "PYTHON". "C" is faster, "PYTHON" is more compatible.
 
-for i in range(100):
-	pix.write(DAT_pin, CLK_pin, num_pixies, str( i/10.0 ), transfer_mode) #counts to from 0 to 10 in steps of 0.1
+# Write string, wait 3 seconds
+pix.write(num_pixies, "TEST")
+time.sleep(3)
+
+# Counts to from 0 to 99.9 in steps of 0.1
+for i in range(1000):
+	pix.write(num_pixies, str( i/10.0 ))
